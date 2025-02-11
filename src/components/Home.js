fd4b40c855db12779d6bf47ffb3836192a2cf0e2
פרל ברכה נדל <pn0556781949@gmail.com>
2025-02-11 16:04:01 +0200

    third

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,24 @@
 
-export default function Home({ a }) {
+export default function Home() {
+  
+      const [showComponents, setShowComponents] = useState("");
+
+    // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (stateApartments) {
+  //       setShowComponentA(!showComponentA);
+  //       if (showComponentA) setShowComponentB(!showComponentB);
+  //     } else setShowComponentA(true);
+  //   }, 9000);
+
+  //   return () => clearInterval(interval);
+  // }, [showComponentA]);
+return({
+
+
+
+
+})
+
 
 }
